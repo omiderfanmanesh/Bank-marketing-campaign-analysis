2ba03d93d0f6aac4d4684c95ff64886b391c9aa1
omid <omiderfanmanesh.dev@gmail.com>
2021-05-20 17:27:18 +0200

    add brief description about dataset

--- data/Bank.py
+++ data/Bank.py
@@ -1,6 +1,8 @@
-import BasedDataset
+from .BasedDataset import BasedDataset
+from utils.FileType import FileType
 
 
 class Bank(BasedDataset):
-    def __init__(self, address, target):
-        super(Bank, self).__init__(True, address, target)
+    def __init__(self, dataset_address, target, dataset_description_file):
+        super(Bank, self).__init__(dataset_type=FileType.CSV, dataset_address=dataset_address, target=target,
+                                   dataset_description_file=dataset_description_file)
--- data/BasedDataset.py
+++ data/BasedDataset.py
@@ -2,40 +2,68 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 import random
+from pprint import pprint
+from utils.FileType import FileType
 
 seed = 2021
 np.random.seed(seed)
 random.seed(seed)
+pd.set_option('display.max_columns', None)  # or 1000
+pd.set_option('display.max_rows', None)  # or 1000
+pd.set_option('display.max_colwidth', None)
 
 
 class BasedDataset:
 
-    def __init__(self, is_csv=True, address=None, target=None):
-        self.is_csv = is_csv
-        self.address = address
+    def __init__(self, dataset_type, dataset_address=None, target=None, dataset_description_file=None):
+        self.dataset_type = dataset_type
+        self.dataset_address = dataset_address
         self.target = target
+        self.dataset_description_file = dataset_description_file
 
-    def load(self):
-        if self.is_csv:
+        if self.dataset_description_file is not None:
+            self.about = self.open_txt_file(self.dataset_description_file)
+
+        self.load_dataset()
+
+    def load_dataset(self):
+        if self._dataset_type == FileType.CSV:
             self.df = self.create_csv_dataframe()
         else:
             raise ValueError('dataset should be CSV file')
-
-        return self.df
 
     def head(self):
         self.df.head()
 
     def description(self):
-        print("--------------- description ---------------")
-        print(self.df.describe().T)
-        print("-------------------------------------------")
-        print("--------------- nan Values ----------------")
-        print(self.is_nan())
-        print("-------------------------------------------")
-        print("--------------- duplicates ----------------")
-        print(self.duplicates())
-        print("-------------------------------------------")
+        if self.dataset_description_file is not None:
+            print("--------------- about dataset  -----------------")
+            print(self.about)
+            print('\n')
+
+        print("--------------- description.txt ----------------")
+        pprint(self.df.describe().T)
+        print('\n')
+
+        print("--------------- nan Values -----------------")
+        pprint(self.is_nan())
+        print('\n')
+
+        print("--------------- duplicates -----------------")
+        print('number of duplicates: ', self.duplicates())
+        print('\n')
+        print("------ Numerical/Categorical Features ------")
+        print('Numerical Features: {}'.format(self.numerical_features()))
+        print('number of Numerical Features: {}'.format(self.numerical_features().__len__()))
+        print('Categorical Features: {}'.format(self.categorical_features()))
+        print('number of Categorical Features: {}'.format(self.categorical_features().__len__()))
+        print('\n')
+
+    def categorical_features(self):
+        return self.df.select_dtypes(include=['object']).columns.tolist()
+
+    def numerical_features(self):
+        return self.df.select_dtypes(exclude=['object']).columns.tolist()
 
     def is_nan(self):
         num_nan = self.df.isna().sum()
@@ -61,36 +89,55 @@ class BasedDataset:
             return X_train, X_test, y_train, y_test
 
     def create_csv_dataframe(self):
-        return pd.read_csv(self.address)
+        return pd.read_csv(self.dataset_address, delimiter=';')
+
+    def open_txt_file(self, desc):
+        return open(desc, 'r').read()
 
     @property
     def df(self):
-        return self.df
+        return self._df
 
     @df.setter
     def df(self, df):
         self._df = df
 
     @property
-    def address(self):
-        return self.address
+    def dataset_address(self):
+        return self._dataset_address
 
-    @address.setter
-    def address(self, address):
-        self.address = address
+    @dataset_address.setter
+    def dataset_address(self, address):
+        self._dataset_address = address
 
     @property
-    def is_csv(self):
-        return self.is_csv
+    def dataset_type(self):
+        return self._dataset_type
 
-    @is_csv.setter
-    def is_csv(self, is_csv):
-        self.is_csv = is_csv
+    @dataset_type.setter
+    def dataset_type(self, type):
+        self._dataset_type = type
 
     @property
     def target(self):
-        return self.target
+        return self._target
 
     @target.setter
     def target(self, target):
-        self.target = target
+        self._target = target
+
+    @property
+    def about(self):
+        return self._about
+
+    @about.setter
+    def about(self, about):
+        self._about = about
+
+    @property
+    def dataset_description_file(self):
+        return self._dataset_description_file
+
+    @dataset_description_file.setter
+    def dataset_description_file(self, value):
+        self._dataset_description_file = value
