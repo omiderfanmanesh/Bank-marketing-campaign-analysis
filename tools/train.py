b6ee7fc18a746a1bd76fc01893bb48f358e27a89
omid <omiderfanmanesh.dev@gmail.com>
2021-05-25 21:35:32 +0200

    add based_plots.py add based_preprocessing.py

---
+++ tools/train.py
@@ -0,0 +1,9 @@
+from engine.trainer import do_train
+
+
+def main():
+    do_train()
+
+
+if __name__ == '__main__':
+    main()
--- utils/based_plots.py
+++ utils/based_plots.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -10,6 +12,12 @@ class BasedPlot:
     def __init__(self, dataset, cfg):
         self._dataset = dataset
         self._df = dataset.df
+
+    def __categorical_features(self):
+        return self.df.select_dtypes(include=['object']).columns.tolist()
+
+    def __numerical_features(self):
+        return self.df.select_dtypes(exclude=['object']).columns.tolist()
 
     def __transform(self, data, trans_type):
         try:
@@ -24,9 +32,60 @@ class BasedPlot:
 
         return data
 
+    def __scatter_plot(self, df, features, trans=None):
+        g = sns.PairGrid(df, vars=features, hue=self.target)
+        g.map_diag(sns.histplot)
+        g.map_offdiag(sns.scatterplot)
+        plt.show()
+
+    def __plot_feature_distribution(self, df, col, is_numerical=True, trans=None):
+        df[col] = self.__transform(df[col], trans)
+        if is_numerical:
+            g = sns.FacetGrid(df, col=self.target, hue=self.target)
+            g.map(sns.histplot, col)
+        else:
+            sns.countplot(data=df, x=col)
+        plt.show()
+
+    def numerical_features_distribution(self, trans=None):
+        features = self.__numerical_features()
+        for f in features:
+            self.__plot_feature_distribution(self.df.copy(), f, is_numerical=True, trans=trans)
+
+    def categorical_features_distribution(self, trans=None):
+        features = self.__categorical_features()
+        for f in features:
+            self.__plot_feature_distribution(self.df.copy(), f, is_numerical=False, trans=trans)
+
+    def numerical_features_scatter_plot(self, trans=None):
+        features = self.__numerical_features()
+        self.__scatter_plot(self.df.copy(), features=features, trans=trans)
+
+    def numerical_features_box_plot(self, trans=None):
+        features = self.__numerical_features()
+        for f in features:
+            self.box_plot_by_col(col=f, trans=trans)
+
+    def numerical_features_violin_plot(self, trans=None):
+        features = self.__numerical_features()
+        for f in features:
+            self.violin_plot_by_col(col=f, trans=trans)
+
     def __dist_plot(self, params):
         sns.displot(**params)
         plt.show()
+
+    def __dist_sub_plot(self, df):
+        sub = ceil(len(self.df.columns) / 3)
+        fig, axes = plt.subplots(nrows=sub, ncols=3)
+        axes = axes.flatten()
+        for ax, col in zip(axes, df.columns):
+            sns.displot(df[col], ax=ax)
+        plt.show()
+
+    def dist_plot_numerical_columns(self):
+        numerical = self.__numerical_features()
+        self.__dist_sub_plot(self.df[numerical])
 
     def dist_plot_by_col(self, col, trans=None):
         df = self.df.copy()
