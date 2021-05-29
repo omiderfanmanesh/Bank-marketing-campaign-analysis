99d1c08624ffd9896336d40f8463a455e23724c9
omid <omiderfanmanesh.dev@gmail.com>
2021-05-29 10:42:17 +0200

    add scaler

--- data/based/__init__.py
+++ data/based/__init__.py
@@ -1,4 +1,5 @@
 from .based_dataset import BasedDataset
 from .encoder_enum import EncoderTypes
 from .file_types import FileTypes
+from .scale_types import ScaleTypes
 from .transformers_enums import TransformersType
--- data/based/based_dataset.py
+++ data/based/based_dataset.py
@@ -9,10 +9,12 @@ from category_encoders import OneHotEncoder, OrdinalEncoder, BinaryEncoder
 from scipy import stats
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, RobustScaler, StandardScaler
 from tqdm import tqdm
 
 from data.based.encoder_enum import EncoderTypes
 from data.based.file_types import FileTypes
+from data.based.scale_types import ScaleTypes
 from data.based.transformers_enums import TransformersType
 
 seed = 2021
@@ -34,14 +36,14 @@ class BasedDataset:
         self.dataset_description_file = cfg.DATASET.DATASET_BRIEF_DESCRIPTION
 
         if self.dataset_description_file is not None:
-            self.about = self.open_txt_file(self.dataset_description_file)
+            self.about = self.__open_txt_file(self.dataset_description_file)
 
         self.load_dataset()
         self.origin_df = self.df.copy()
 
     def load_dataset(self):
         if self.dataset_type == FileTypes.CSV:
-            self.df = self.create_csv_dataframe()
+            self.df = self.__create_csv_dataframe()
         else:
             raise ValueError('dataset should be CSV file')
 
@@ -58,13 +60,13 @@ class BasedDataset:
 
         return data
 
-    def categorical_features(self, data=None):
+    def __categorical_features(self, data=None):
         if data is None:
             return self.df.select_dtypes(include=['object']).columns.tolist()
         else:
             return data.select_dtypes(include=['object']).columns.tolist()
 
-    def numerical_features(self, data=None):
+    def __numerical_features(self, data=None):
         if data is None:
             return self.df.select_dtypes(exclude=['object']).columns.tolist()
         else:
@@ -72,7 +74,7 @@ class BasedDataset:
 
     def select_columns(self, data, cols=None, just_numerical=True):
         if cols is None and just_numerical:
-            cols = self.numerical_features(data=data)
+            cols = self.__numerical_features(data=data)
         return data[cols]
 
     def __target_encoding(self):
@@ -80,7 +82,7 @@ class BasedDataset:
         targets = le.fit_transform(self.df[self.target])
         return targets
 
-    def categorical_feature_encoder(self, encoder_type, col):
+    def __get_encoder(self, encoder_type, col):
         le = None
         le_name = None
         if encoder_type == EncoderTypes.LABEL:
@@ -98,11 +100,11 @@ class BasedDataset:
 
         return le, le_name
 
-    def categorical_feature_mapping(self, col, mapping_value):
-        new_col = self.generate_new_column_name(col=col, prefix='mapping')
+    def __categorical_feature_mapping(self, col, mapping_value):
+        new_col = self.__generate_new_column_name(col=col, prefix='mapping')
         self.df[new_col] = self.df[col].map(mapping_value)
 
-    def encoder(self, enc, X_train, X_test=None, y_train=None, y_test=None):
+    def __encoder(self, enc, X_train, X_test=None, y_train=None, y_test=None):
         if isinstance(enc, LabelEncoder):
             enc.fit(X_train)
             train_enc = enc.transform(X_train)
@@ -121,42 +123,51 @@ class BasedDataset:
         for col in tqdm(self._cfg.ENCODER):
             encode_type = self._cfg.ENCODER[col]
             col = col.lower()
-            enc, enc_name = self.categorical_feature_encoder(encoder_type=encode_type, col=col)
+            enc, enc_name = self.__get_encoder(encoder_type=encode_type, col=col)
             if encode_type == EncoderTypes.LABEL:
                 train_val = X_train[col].values
                 test_val = X_test[col].values
-                X_train[col], X_test[col] = self.encoder(enc=enc, X_train=train_val, X_test=test_val)
+                X_train[col], X_test[col] = self.__encoder(enc=enc, X_train=train_val, X_test=test_val)
             else:
-                X_train, X_test = self.encoder(enc=enc, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
+                X_train, X_test = self.__encoder(enc=enc, X_train=X_train, X_test=X_test, y_train=y_train,
+                                                 y_test=y_test)
         return X_train, X_test
 
-    def generate_new_column_name(self, col, prefix):
-        return '{}_{}'.format(col, prefix)
+    def __get_scaler(self, scale_type):
+        scl = None
+        scl_name = None
+        if scale_type == ScaleTypes.MIN_MAX:
+            scl = MinMaxScaler()
+            scl_name = 'min_max_scaler'
+        elif scale_type == ScaleTypes.STANDARD:
+            scl = StandardScaler()
+            scl_name = 'min_max_scaler'
+        elif scale_type == ScaleTypes.MAX_ABS:
+            scl = MaxAbsScaler()
+            scl_name = 'max_abs_scaler'
+        elif scale_type == ScaleTypes.ROBUST:
+            scl = RobustScaler()
+            scl_name = 'robust_scaler'
 
-    def samples_and_labels(self):
-        data = self.df.copy()
+        return scl, scl_name
 
-        y = self.__target_encoding()
-        X = data.drop(self.target, axis=1)
+    def __scaler(self, scl, X_train, X_test=None):
+        scl.fit(X_train)  # Apply transform to both the training set and the test set.
+        train_scale = scl.transform(X_train)
+        test_scale = None
+        if X_test is not None:
+            test_scale = scl.transform(X_test)
 
-        return X, y
+        return train_scale, test_scale
 
-    def create_csv_dataframe(self):
-        return pd.read_csv(self.dataset_address, delimiter=';')
-
-    def open_txt_file(self, desc):
-        return open(desc, 'r').read()
-
-    def scaler(self, scl, train, test=None):
-        scl.fit(train)  # Apply transform to both the training set and the test set.
-        train_scale = scl.transform(train)
-        if test is not None:
-            test_scale = scl.fit_transform(test)
-
-        return train_scale, test_scale, scl
+    def do_scale(self, X_train, X_test):
+        scl_type = self._cfg.SCALER
+        scl, scl_name = self.__get_scaler(scale_type=scl_type)
+        train_scale, test_scale = self.__scaler(scl=scl, X_train=X_train, X_test=X_test)
+        return train_scale, test_scale
 
     def split_to(self, test_size=0.10, val_size=0.10, has_validation=False, random_state=seed):
-        X, y = self.samples_and_labels()
+        X, y = self.__samples_and_labels()
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
         if has_validation:
             X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_size,
@@ -164,6 +175,23 @@ class BasedDataset:
             return X_train, X_val, X_test, y_train, y_val, y_test
         else:
             return X_train, X_test, y_train, y_test
+
+    def __generate_new_column_name(self, col, prefix):
+        return '{}_{}'.format(col, prefix)
+
+    def __samples_and_labels(self):
+        data = self.df.copy()
+
+        y = self.__target_encoding()
+        X = data.drop(self.target, axis=1)
+
+        return X, y
+
+    def __create_csv_dataframe(self):
+        return pd.read_csv(self.dataset_address, delimiter=';')
+
+    def __open_txt_file(self, desc):
+        return open(desc, 'r').read()
 
     @property
     def df(self):
