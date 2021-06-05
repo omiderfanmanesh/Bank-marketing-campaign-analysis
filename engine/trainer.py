1b335951d26a0d6d090e0f8977c0fc4344a84774
omid <omiderfanmanesh.dev@gmail.com>
2021-06-05 10:52:18 +0200

    add analysis.py to tools directory in order to run do_analysis() method

--- engine/trainer.py
+++ engine/trainer.py
@@ -1,13 +1,13 @@
 import warnings
 
-from data.bank import Bank
+from data.based.based_dataset import BasedDataset
 from data.preprocessing import Encoders, Scalers, PCA
 from model.based import BasedModel
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-def do_train(cfg, model: BasedModel, dataset: Bank, encoder: Encoders, scaler: Scalers, pca: PCA):
+def do_train(cfg, model: BasedModel, dataset: BasedDataset, encoder: Encoders, scaler: Scalers, pca: PCA):
     if pca is None:
         X_train, X_test, y_train, y_test = dataset.split_to()
         X_train, X_test = encoder.do_encode(X_train=X_train, X_test=X_test, y_train=y_train,
